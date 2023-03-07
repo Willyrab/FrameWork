97a6235422bbafd5f9dc72e76f470e962d7ea8cc
Willyrab <119251355+Willyrab@users.noreply.github.com>
2023-03-07 17:40:21 +0300

    s

---
+++ FrontServlet.java
@@ -0,0 +1,17 @@
+package etu001799.framework.servlet;
+
+
+
+
+
+import java.io.*;
+import java.util.HashMap;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class FrontServlet extends HttpServlet {
+
+    
+}
